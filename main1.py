e4f571c885cd9a09004641da20e03502e22e6057
baraban888 <1975bahat@gmail.com>
2025-08-25 16:58:13 +0200

    Add purchase output

--- main1.py
+++ main1.py
@@ -5,3 +5,5 @@ zakupy = {
 for sklep, produkty in zakupy.items():
     print(f"Idę do {sklep}, kupuję tu następujące rzeczy: {produkty}")
 
+suma = sum(len(produkty) for produkty in zakupy.values())
+print(f"W sumie kupuję {suma} produktów.")
