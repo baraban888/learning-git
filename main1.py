ae05d57c0acdd6df5e337a05d7ebcc0fe06014e6
baraban888 <1975bahat@gmail.com>
2025-08-25 16:55:35 +0200

    Added loop for

--- main1.py
+++ main1.py
@@ -2,5 +2,6 @@ zakupy = {
     "Piekarnia": ["Chleb", "Pączek", "Bułki"],
     "Warzywniak": ["Marchew", "Seler", "Rukola"]
 }
-
+for sklep, produkty in zakupy.items():
+    print(f"Idę do {sklep}, kupuję tu następujące rzeczy: {produkty}")
 
