528efaf536f7c89e5e142c4536b5de7523fb8bb0
baraban888 <1975bahat@gmail.com>
2025-09-19 17:48:39 +0200

    Add new fail

--- main1.py
+++
@@ -1,9 +0,0 @@
-zakupy = {
-    "Piekarnia": ["Chleb", "Pączek", "Bułki"],
-    "Warzywniak": ["Marchew", "Seler", "Rukola"]
-}
-for sklep, produkty in zakupy.items():
-    print(f"Idę do {sklep}, kupuję tu następujące rzeczy: {produkty}")
-
-suma = sum(len(produkty) for produkty in zakupy.values())
-print(f"W sumie kupuję {suma} produktów.")
